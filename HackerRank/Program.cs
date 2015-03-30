e75274f6b6f6f0f7ca20700c57bea7a00053b230
somebody1234 <ehern081700@hotmail.com>
2015-03-30 19:38:24 +1000

    Optimisation Part 1

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,254 +1,164 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
-namespace HangmanCode
+namespace HangmanGame
 {
-    class HangmanLogic
+    internal class HangmanLogic
     {
-        static int hangmanGraphicsID;
-        static char[] alphabetCharArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-        static char[] lettersGuessed = new char[alphabetCharArray.Length];
-        static char[] guessedCharArray;
+        static readonly string[] Graphics = {"", "", "", "", "", ""};
+        private static int wrongGuesses;
 
-        static void Main()
+        private static readonly char[] AlphabetCharArray =
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+
+        private static readonly char[] LettersGuessed = new char[26];
+        private static char[] guessedCharArray;
+
+        private static void Main()
         {
             Console.Clear();
-            hangmanGraphicsID = -1;
-            OutputGraphics(10);
+            wrongGuesses = -1;
+            FinalGraphics();
 
             Console.WriteLine("\nType a word that you would like to guess in this game");
-            string stringToGuess = Console.ReadLine().ToUpper();
+            var readLine = Console.ReadLine();
+            if (readLine == null) return;
+            var stringToGuess = readLine.ToUpper();
             CheckWordSupport(stringToGuess);
 
             guessedCharArray = new char[stringToGuess.Length];
-            ReplaceCharArray(lettersGuessed, '_');
+            ReplaceCharArray(LettersGuessed, '_');
             ReplaceCharArray(guessedCharArray, '_');
             ReplaceGuessWithSpace(stringToGuess);
-
             Console.Clear();
-            OutputGraphics(10);
-
-            SolveHangman(stringToGuess, guessedCharArray);
+            FinalGraphics();
+            SolveHangman(stringToGuess);
         }
 
-        static void SolveHangman(string stringToGuess, char[] guessedCharArray)
+        private static void SolveHangman(string stringToGuess)
         {
             Console.WriteLine("\nType a letter, and see if you can guess my word. Good Luck");
-            char guessedChar = Char.ToUpper(Console.ReadKey().KeyChar);
-            if (!Char.IsLetter(guessedChar))
+            while (true)
             {
-                PrintError("The character you provided does not classify as a letter");
-            }
-            Console.Clear();
+                var guessedChar = Char.ToUpper(Console.ReadKey(true).KeyChar);
+                if (!Char.IsLetter(guessedChar)) continue;
+                
+                var guessIsEdited = false;
 
-            bool isGuessEdited = false;
-
-            for (int i = 0; i < stringToGuess.Length; i++)
-            {
-                if (!Char.IsLetter(guessedChar))
+                if (!Char.IsLetter(guessedChar) || CheckIfTyped(guessedChar)) guessIsEdited = true;
+                else for (var i = 0; i < stringToGuess.Length; i++)
                 {
-                    isGuessEdited = true;
-                    break;
-                }
-                if (CheckIfTyped(guessedChar))
-                {
-                    isGuessEdited = true;
-                    break;
-                }
-                if (stringToGuess[i] == guessedChar)
-                {
+                    if (stringToGuess[i] != guessedChar) continue;
+                    guessIsEdited = true;
                     guessedCharArray[i] = guessedChar;
-                    isGuessEdited = true;
                 }
+                CheckIfWon(stringToGuess);
+                ChangeCharOnType(guessedChar);
+                if (guessIsEdited)
+                {
+                    if (stringToGuess.All(t => t != guessedChar)) wrongGuesses++;
+                    Console.Clear();
+                    OutputGraphics(wrongGuesses);
+                    OutputCharArray(guessedCharArray);
+                    OutputCharArray(LettersGuessed);
+                }
+                Console.WriteLine();
             }
-
-            CheckIfWon(stringToGuess);
-
-            if (!isGuessEdited)
-            {
-                hangmanGraphicsID++;
-            }
-
-            ChangeCharOnType(guessedChar);
-            OutputGraphics(hangmanGraphicsID);
-
-            OutputCharArray(guessedCharArray);
-            OutputCharArray(lettersGuessed);
-            Console.WriteLine();
-            SolveHangman(stringToGuess, guessedCharArray);
         }
 
-        static void EndResult(string wordGuessed, string outcome, char[] yourGuess)
+        private static void EndResult(string wordGuessed, string outcome)
         {
             Console.Clear();
-            OutputGraphics(hangmanGraphicsID);
-            OutputCharArray(guessedCharArray);
-            OutputCharArray(lettersGuessed);
-            Console.WriteLine("\n\nYou {0}, the word was {1}!\nPress any key to try again, or press N to close", outcome, wordGuessed);
-            if (Char.ToUpper(Console.ReadKey().KeyChar) == 'N')
+            Console.CursorTop = 10;
+            Console.WriteLine("\n\nYou {0}, the word was {1}!\nPress any key to try again, or press n to close", outcome, wordGuessed);
+            if (Console.ReadKey(true).KeyChar == 'n')
             {
                 Console.WriteLine("Thanks for playing!");
+                Console.ReadKey(true); // So people can read it
                 Environment.Exit(0);
             }
             else
             {
                 Console.Clear();
+                for (var i = 0; i < 6; i++) Graphics[i] = "";
                 Main();
             }
         }
 
-        static void ReplaceGuessWithSpace(string stringToGuess)
-        {
-            for (int i = 0; i < stringToGuess.Length; i++)
-            {
-                if (stringToGuess[i] == ' ')
-                {
-                    guessedCharArray[i] = ' ';
-                }
-            }
-        }
+        private static void ReplaceGuessWithSpace(string stringToGuess) { for (var i = 0; i < stringToGuess.Length; i++) if (stringToGuess[i] == ' ') guessedCharArray[i] = ' '; }
 
-        static void PrintError(string error)
+        private static void PrintError(string error)
         {
             Console.Clear();
-            OutputGraphics(10);
-
+            FinalGraphics();
             Console.WriteLine("\nAn error has occured in the game\nError: {0}\nPress any key to try again", error);
-            Console.ReadKey();
+            Console.ReadKey(true);
             Main();
         }
 
-        static void ChangeCharOnType(char guessedChar)
+        private static void ChangeCharOnType(char guessedChar) { for (var i = 0; i < AlphabetCharArray.Length; i++) if (AlphabetCharArray[i] == guessedChar) LettersGuessed[i] = guessedChar; }
+
+        private static void ReplaceCharArray(char[] charArray, char charToReplace) { for (var i = 0; i < charArray.Length; i++) charArray[i] = charToReplace; }
+
+        private static void CheckIfWon(string stringToGuess)
         {
-            for (int i = 0; i < alphabetCharArray.Length; i++)
-            {
-                if (alphabetCharArray[i] == guessedChar)
-                {
-                    lettersGuessed[i] = guessedChar;
-                }
-            }
+            if (stringToGuess == new string(guessedCharArray)) EndResult(stringToGuess, "won");
+            else if (wrongGuesses == 10) EndResult(stringToGuess, "lost");
         }
 
-        static void ReplaceCharArray(char[] charArray, char charToReplace)
+        private static bool CheckIfTyped(char guessedChar) { return LettersGuessed.Any(t => guessedChar == t); }
+
+        private static void CheckWordSupport(string stringToGuess)
         {
-            for (int i = 0; i < charArray.Length; i++)
+            var areThereLetters = false;
+
+            for (var i = 0; i < stringToGuess.Length; i++)
             {
-                charArray[i] = charToReplace;
+                if (!areThereLetters && stringToGuess[i] != ' ') areThereLetters = true;
+                if (!Char.IsLetter(stringToGuess[i]) && stringToGuess[i] != ' ') PrintError("The word you provided include characters that do not classify as letters");
+                if (stringToGuess[i] != ' ' || stringToGuess[i] != stringToGuess[i + 1]) continue;
+                stringToGuess = stringToGuess.Remove(i + 1, 1);
+                i--;
             }
+
+            if (!areThereLetters) PrintError("The word you provided consists entirely of spaces.");
         }
 
-        static void CheckIfWon(string stringToGuess)
+        private static void OutputCharArray(char[] charArray) { Console.WriteLine(); foreach (var element in charArray) Console.Write(" {0} ", element); }
+
+        private static void FinalGraphics()
         {
-            if (stringToGuess == new string(guessedCharArray))
-            {
-                EndResult(stringToGuess, "won", guessedCharArray);
-            }
-            else if (hangmanGraphicsID == 10)
-            {
-                EndResult(stringToGuess, "lost", guessedCharArray);
-            }
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}",
+            @" __________",
+            @"| /      |",
+            @"|/       0",
+            @"|       /|\",
+            @"|       / \",
+            @"|_______________");
         }
 
-        static bool CheckIfTyped(char guessedChar)
+        private static void OutputGraphics(int graphicsId)
         {
-            for (int i = 0; i < lettersGuessed.Length; i++)
+            if (graphicsId == 0) Graphics[5] = " _______________";
+            if (graphicsId == 1)
             {
-                if (guessedChar == lettersGuessed[i])
-                {
-                    return true;
-                }
+                for (var i = 1; i < 5; i++) Graphics[i] += "|";
+                Graphics[5] = Graphics[5].Remove(0, 1).Insert(0, "|"); 
             }
-            return false;
-        }
-
-        static void CheckWordSupport(string stringToGuess)
-        {
-            bool areThereLetters = false;
-
-            for (int i = 0; i < stringToGuess.Length; i++)
-            {
-                if (!areThereLetters && stringToGuess[i] != ' ')
-                {
-                    areThereLetters = true;
-                }
-                if (!Char.IsLetter(stringToGuess[i]) && stringToGuess[i] != ' ')
-                {
-                    PrintError("The word you provided include characters that do not classify as letters");
-                }
-                if (stringToGuess[i] == ' ' && stringToGuess[i] == stringToGuess[i + 1])
-                {
-                    stringToGuess = stringToGuess.Remove(i + 1, 1);
-                    i--;
-                }
-            }
-
-            if (!areThereLetters)
-            {
-                PrintError("The word you provided consists entirely of spaces.");
-            }
-        }
-
-        static void OutputCharArray(char[] charArray)
-        {
-            Console.WriteLine();
-            foreach (char element in charArray)
-            {
-                Console.Write(" {0} ", element);
-            }
-        }
-
-        static void OutputGraphics(int hangmanGraphicsID)
-        {
-            switch (hangmanGraphicsID)
-            {
-                case -1:
-                    Console.WriteLine("\n\n\n\n\n\n");
-                    break;
-
-                case 0:
-                    Console.WriteLine("\n\n\n\n\n\n_________________");
-                    break;
-
-                case 1:
-                    Console.WriteLine("\n|\n|\n|\n|\n|\n|_______________");
-                    break;
-
-                case 2:
-                    Console.WriteLine(" __________\n|\n|\n|\n|\n|\n|_______________");
-                    break;
-
-                case 3:
-                    Console.WriteLine(" ___________\n| /\n|/\n|\n|\n|\n|_______________");
-                    break;
-
-                case 4:
-                    Console.WriteLine(" ___________\n| /        |\n|/\n|\n|\n|\n|\n|_______________");
-                    break;
-
-                case 5:
-                    Console.WriteLine(" ___________\n| /        |\n|/         0\n|\n|\n|\n|_______________");
-                    break;
-
-                case 6:
-                    Console.WriteLine(" ___________\n| /        |\n|/         0\n|          |\n|\n|\n|_______________");
-                    break;
-
-                case 7:
-                    Console.WriteLine(" ___________\n| /        |\n|/         0\n|         /|\n|\n|\n|_______________");
-                    break;
-
-                case 8:
-                    Console.WriteLine(" ___________\n| /        |\n|/         0\n|         /|\\\n|\n|\n|_______________");
-                    break;
-
-                case 9:
-                    Console.WriteLine(" ___________\n| /        |\n|/         0\n|         /|\\\n|         /\n|\n|_______________");
-                    break;
-
-                case 10:
-                    Console.WriteLine(" ___________\n| /        |\n|/         0\n|         /|\\\n|         / \\\n|\n|_______________");
-                    break;
-            }
+            if (graphicsId == 2) Graphics[0] = " __________";
+            if (graphicsId == 3) { Graphics[1] += " /"; Graphics[2] += "/"; }
+            if (graphicsId == 4) Graphics[1] += "        |";
+            if (graphicsId == 5) Graphics[2] += "         0";
+            if (graphicsId == 6) Graphics[3] += "          |";
+            if (graphicsId == 7) { Graphics[3] = Graphics[3].Remove(10, 1).Insert(10, "/"); }
+            //TODO: optimise
+            if (graphicsId == 8) Graphics[3] += @"\";
+            if (graphicsId == 9) Graphics[4] += "         /";
+            if (graphicsId == 10) Graphics[4] += " \\";
+            for (var i = 0; i < 6; i++) Console.WriteLine(Graphics[i]);
         }
     }
 }
